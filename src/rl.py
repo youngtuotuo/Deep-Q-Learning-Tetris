b75271966967bc24ded09f19a11bcaf943f4af83
Tuo Hung <youngtuotuo@protonmail.com>
2022-10-24 16:29:20 +0800

    initial network

--- src/rl.py
+++ src/rl.py
@@ -1,11 +1,27 @@
-from tetris import main, cnn_main, cols, rows, RIGHT, LEFT, UP, DOWN, NOTHING, values
-from tetris import window
+import torch.nn as nn
 
-import numpy as np
-import random
-import pygame
-from pygame.locals import *
+class DeepQNetwork(nn.Module):
+    def __init__(self):
+        super(DeepQNetwork, self).__init__()
 
+        self.conv1 = nn.Sequential(nn.Linear(4, 64), nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Linear(64, 1))
+
+        self._create_weights()
+
+    def _create_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+
+        return x
 
 if __name__ == '__main__':
     pass
